ee9023a372d3cfa1ee159a54bbe1d0d0eea7b52f
16nicolha <100052466+16nicolha@users.noreply.github.com>
2025-06-25 23:46:13 -0400

    added beginning of nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css"
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
         <div className = "blur" style={{top: '-18%', right: '0'}}></div>
         <div className = "blur" style={{top: '36%', right: '-8rem'}}></div>
+        <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div class = "Home">
+            <div class="profileSide">Profile</div>
+            <div class="postSide">Posts</div>
+            <div class="RightSide">Rightside</div>
+        </div>
+    )
+
+}
+
+export default Home
