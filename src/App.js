239c01403d9795df7acade58a4bfd8c7c7394018
16nicolha <100052466+16nicolha@users.noreply.github.com>
2025-06-25 23:13:51 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        Test Text
+        <div className = "blur" style={{top: '-18%', right: '0'}}></div>
+        <div className = "blur" style={{top: '36%', right: '-8rem'}}></div>
     </div>
   );
 }
