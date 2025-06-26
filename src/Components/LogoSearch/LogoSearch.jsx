6f2c0a3e03ed5b3e872e430f8c3c9dde1f92a7c0
16nicolha <100052466+16nicolha@users.noreply.github.com>
2025-06-26 14:42:17 -0400

    search bar css

--- src/Components/LogoSearch/LogoSearch.jsx
+++ src/Components/LogoSearch/LogoSearch.jsx
@@ -1,10 +1,20 @@
 import react from 'react'
-
+import Logo from '../../img/logo.png'
+import {UilSearch} from '@iconscout/react-unicons'
+import './Logosearch.css'
 const LogoSearch = () => {
     return (
-        <div>LogoSearch</div>
+        <div class = "LogoSearch">
+                <img src ={Logo}alt=""/>
+                    <div class ="Search">
+                        <input type = "text" placeholder= '#Explore'></input>
+                        <div class="s-icon">
+                            <UilSearch/>
+                        </div>
+                    </div>
+                        
+        </div>
     )
-
 }
 
 export default LogoSearch
